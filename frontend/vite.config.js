6a910d30d18c3d15672c33c566e421ec2849d436
BHigh17 <brandonalexh@comcast.net>
2024-12-07 14:46:25 -0500

    frontend dependency

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
             output: {
                 manualChunks: {
                     vendor: ['react', 'react-dom', 'react-router-dom'],
-                    ui: ['lucide-react', '@radix-ui/react-alert-dialog']
+                    ui: ['lucide-react', '@radix-ui/react-alert-dialog', '@radix-ui/react-tabs']
                 }
             },
             external: [
